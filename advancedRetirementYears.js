057f23e7ae7a941c6f0cdc026f05102d7ea96e63
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-01-24 14:39:08 +0300

    example for functions

--- advancedRetirementYears.js
+++ advancedRetirementYears.js
@@ -1,1 +1,22 @@
 'use strict';
+
+
+const calcAge = function(birthYear) {
+    return 2026 - birthYear;
+}
+
+const yearsUntilRetirement = function(birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log `${firstName} retires in ${retirement} years`
+        return retirement;
+    } else {
+        console.log`${firstName} is retired`;
+        return -1;
+    }
+
+}
+
+console.log(yearsUntilRetirement("1900", "Bob"));
