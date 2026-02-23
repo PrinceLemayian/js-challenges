ee22f98d1214e3a4e37c56a6986a29c93a75abb4
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-02-23 16:47:07 +0300

    added more examples for object methods

---
+++ objMethods.js
@@ -0,0 +1,24 @@
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtman',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function() {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function() {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    }
+
+};
+
+console.log(jonas.calcAge());
+console.log(jonas.age);
+console.log(jonas.getSummary());
+
+// console.log(jonas['calcAge'](1991));
