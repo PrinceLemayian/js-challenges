6c28f2bb333b1d031f2843b06f4541dd1bb35fb3
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-18 23:47:05 +0300

    add challenge #3

--- footballBetting.js
+++ footballBetting.js
@@ -93,3 +93,20 @@ for (const scorer of game.scored) {
 }
 
 console.log(scorers);
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = new Set([...gameEvents.values()]);
+console.log(events);
