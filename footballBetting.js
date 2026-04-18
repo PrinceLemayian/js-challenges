1d6bb7f61bc3678a972a42134b6c3a3ded22986b
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-19 00:36:38 +0300

    add final solution(s) for footballBetting challenge

--- footballBetting.js
+++ footballBetting.js
@@ -108,5 +108,21 @@ const gameEvents = new Map([
   [92, '🔶 Yellow card'],
 ]);
 
-const events = new Set([...gameEvents.values()]);
+// 1
+const events = [...new Set(gameEvents.values())];
+gameEvents.delete(64);
+
+// 2
+const time = [...gameEvents.keys()].pop();
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`,
+);
+
+// 3
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+
+console.log(gameEvents);
 console.log(events);
