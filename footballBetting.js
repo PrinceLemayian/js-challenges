40c6534d06282df9fed948a48d10ffb5f7e92146
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-19 21:39:39 +0300

    final solution for footballBetting challenge

--- footballBetting.js
+++ footballBetting.js
@@ -123,6 +123,3 @@ for (const [min, event] of gameEvents) {
   const half = min <= 45 ? 'FIRST' : 'SECOND';
   console.log(`[${half} HALF] ${min}: ${event}`);
 }
-
-console.log(gameEvents);
-console.log(events);
